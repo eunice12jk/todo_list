3425be1433143bd6587293fb914c0063fded4a0d
eunice12jk <euchiri@gmail.com>
2017-05-27 13:59:32 +0300

    Part 3. Mail catcher.

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,11 +5,9 @@ class Task < ActiveRecord::Base
   private
 
   def future_completed_date
-    if !completed.blank? Date.today
+    if !completed.blank? && completed > Date.today
       self.errors.add(:completed, "can't be in the future")
     end
   end
-
 end
-
 
